70d9331a43fb60ed007e77fdbdd16be4bdf33071
yoGerto <conorfdenby@gmail.com>
2024-08-19 13:54:19 +0100

    Custom UI values retained, Child script now handles audio
    The values selected for 'play mode' and such are now correctly retained within the UI. Previously they would disappear when selecting another object and returning to the script, or when closing and reopening the program. These values are also now passed into the scripts for use.
    This commit also contains the changes to the Audio playing sequence to make it so that the audio is played from the Child Object. The plan is to have the child object with manual sync method play the sounds as it is more responsive that way. The multiple sounds method was implemented and tested first, which is working as of this commit. Overall progress is good and not much is left to be done to get it completely working.

--- SimpleAudioPlayer.cs
+++ SimpleAudioPlayer.cs
@@ -10,6 +10,8 @@ using UnityEditor;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
 using System;
+using UnityEditorInternal;
+
 
 #if !COMPILER_UDONSHARP && UNITY_EDITOR
 using UdonSharpEditor;
@@ -17,40 +19,31 @@ using UdonSharpEditor;
 
 public class SimpleAudioPlayer : UdonSharpBehaviour
 {
-    /* Items used within the script */
-    private Animator teddyAnimator;
-    private AudioSource teddyAudio;
-
-    public GameObject emptyGameObject;
-
-    public AudioClip[] teddyClips;
-
-    public AudioClip testAudioClip;
-
-    private float timer = 0.0f;
-    private float cooldownTime = 0.5f;
-    private bool timerLatch = false;
-
-    [NonSerialized] public string stringVal = "glorp";
-    [NonSerialized] public bool godTest = false;
-
-    [UdonSynced] private int RNGValue = 0;
+    public AudioClip[] soundClips;
 
     [SerializeField] public GameObject SAP_Child;
-    [SerializeField] SimpleAudioPlayer_Child SAP_Child_Script;
+    [HideInInspector] public GameObject glorpington;
+    SimpleAudioPlayer_Child SAP_Child_Script;
+
+    public int targetSound = 0;
+    public int m_TabsSerialized = 0;
+    public int[] limits = { 1, 2, 3, 4 };
 
     private void Start()
     {
-        teddyAnimator = GetComponent<Animator>();
-        teddyAudio = GetComponent<AudioSource>();
-        SendCustomEvent("SetChildScriptObjects");
+        Debug.Log(limits[1]);
+        SAP_Child_Script = SAP_Child.GetComponent<SimpleAudioPlayer_Child>();
+        //SendCustomEvent("SetChildScriptObjects");
+        SAP_Child_Script.SetProgramVariable("SAPChild_AudioClips", soundClips);
+        SAP_Child_Script.SetProgramVariable("limits", limits);
     }
 
     public void SetChildScriptObjects()
     {
         Debug.Log("Entered!");
-        SAP_Child.GetComponent<SimpleAudioPlayer_Child>().SetProgramVariable("test", 1);
-        SAP_Child.GetComponent<SimpleAudioPlayer_Child>().SetProgramVariable("SAPChild_AudioClips", testAudioClip);
+        //int[] limits = { 1, 2, 3, 4 };
+        //SAP_Child.GetComponent<SimpleAudioPlayer_Child>().SetProgramVariable("SAPChild_AudioClips", teddyClips);
+        //SAP_Child.GetComponent<SimpleAudioPlayer_Child>().SetProgramVariable("limits", limits);
     }
 
     public override void OnPickup()
@@ -60,55 +53,7 @@ public class SimpleAudioPlayer : UdonSharpBehaviour
 
     public override void OnPickupUseDown()
     {
-        //RNGValue = UnityEngine.Random.Range(0, 200);
-        //SAP_Child.GetComponent<SimpleAudioPlayer_Child>().SetProgramVariable("test2", RNGValue);
         SAP_Child.GetComponent<SimpleAudioPlayer_Child>().SendCustomEvent("GenerateRNGVal");
-        //RNGValue = UnityEngine.Random.Range(0, 200);
-        //Debug.Log(RNGValue.ToString());
-        //RequestSerialization();
-    }
-
-    public void TeddyBearAudioHandler()
-    {
-        int audioRNG;
-        audioRNG = UnityEngine.Random.Range(0, 200);
-        if (audioRNG < 100)
-        {
-            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "PlayTeddyBearAudio0");
-        }
-        else if (audioRNG >= 100 && audioRNG < 199)
-        {
-            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "PlayTeddyBearAudio1");
-        }
-        else
-        {
-            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "PlayTeddyBearAudio2");
-        }
-    }
-
-    public void PlayTeddyBearAudio0()
-    {
-        teddyAudio.PlayOneShot(teddyClips[0]);
-        teddyAnimator.SetInteger("BearState", 1);
-        SendCustomEventDelayedSeconds("ResetAnimatorParam", 0.05f);
-    }
-
-    public void PlayTeddyBearAudio1()
-    {
-        teddyAudio.PlayOneShot(teddyClips[1]);
-        teddyAnimator.SetInteger("BearState", 2);
-        SendCustomEventDelayedSeconds("ResetAnimatorParam", 0.05f);
-    }
-
-    public void PlayTeddyBearAudio2()
-    {
-        teddyAudio.PlayOneShot(teddyClips[2]);
-    }
-
-    public void ResetAnimatorParam()
-    {
-
-        teddyAnimator.SetInteger("BearState", 0);
     }
 
 }
@@ -117,7 +62,6 @@ public class SimpleAudioPlayer : UdonSharpBehaviour
 [CustomEditor(typeof(SimpleAudioPlayer))]
 public class CustomInspectorEditor : Editor
 {
-    bool boolTest1 = false;
 
     GameObject gameObjectTest;
     GameObject instantiatedGameObject;
@@ -126,36 +70,117 @@ public class CustomInspectorEditor : Editor
      * https://www.youtube.com/watch?v=-sJRvRirJ9Q
      */
 
-    private string[] m_Tabs = { "Option One", "Option Two", "Option Three", "Option Four" };
-    private int m_TabsIndex = -1;
+    private string[] m_Tabs = { "Single Sound", "Multiple Sounds - Random", "Multiple Sounds - Sequence", "Option Four" };
+    private int m_TabsIndex = 0;
+
+    int sliderValue = 0;
+
+    int[] limits;
+
+    /* Approach to create an Array List in Custom Editor from this StackOverflow question:
+     * https://stackoverflow.com/questions/47753367/how-to-display-modify-array-in-the-editor-window
+     * Also the Unity Docs were useful in understanding how to properly Serialize properties so the data in the Editor can be stored: 
+     * https://docs.unity3d.com/ScriptReference/Editor.html
+     * ALSO the UdonSharp API docs were used to understanding the differences in implementing this in U# versus Unity's default C#:
+     * https://udonsharp.docs.vrchat.com/editor-scripting/
+     */
+
+    SerializedProperty intTest;
+    SerializedProperty tabIndex_Serialized;
+    SerializedProperty targetSound_Serialized;
+    SerializedProperty soundClips_Serialized;
+
+    private void OnEnable()
+    {
+        // Set up all SerializedProperties in here
+
+        /* I'd like to do more reading into the idea of Serialization, but my understanding of it is:
+         * It is used to make private variables visible in the editor
+         * It is used to make variables more 'permanent', as in when you close the program, the settings chosen in the custom UI remain when it is opened again.
+         */
+
+        intTest = serializedObject.FindProperty("selectedValue");
+        tabIndex_Serialized = serializedObject.FindProperty("m_TabsSerialized");
+        targetSound_Serialized = serializedObject.FindProperty("targetSound");
+        soundClips_Serialized = serializedObject.FindProperty("soundClips");
+    }
 
     public override void OnInspectorGUI()
     {
-        // Draws the default convert to UdonBehaviour button, program asset field, sync settings, etc.
-        if (UdonSharpGUI.DrawDefaultUdonSharpBehaviourHeader(target)) return;
+        // I will not be drawing the default inspector here, I would like to pick and choose which UI elements from the original list I display
 
-        DrawDefaultInspector();
+        // Unity's documentation states that this must be called every time OnInspectorGUI is called, not entirely sure what it does?
+        serializedObject.Update();
+
+        // This line creates a reference to the original script which we are designing the Custom UI for.
+        // It allows us to access variables and properties of the original class
+        SimpleAudioPlayer inspectorBehaviour = (SimpleAudioPlayer)target;
+
+        // This line is nice because it allows us to display that element in the default manner in the Custom UI
+        // Here, the soundClips variable can be displayed in the default manner, which supports dragging and dropping files into the header to add them to the list. Great!
+        EditorGUILayout.PropertyField(soundClips_Serialized, true);
+        serializedObject.ApplyModifiedProperties();
+
+        if (inspectorBehaviour.soundClips.Length == 0)
+        {
+            EditorGUILayout.HelpBox("Drag and drop your audio clips in to begin using the script", MessageType.Info);
+
+            // returning here makes a nice effect where the remainder of the UI is not drawn unless an Audio file has been inserted into the script!
+            return;
+        }
+
+        //DrawDefaultInspector();
 
         EditorGUILayout.BeginVertical();
-        m_TabsIndex = GUILayout.Toolbar(m_TabsIndex, m_Tabs);
+        tabIndex_Serialized.intValue = GUILayout.Toolbar(tabIndex_Serialized.intValue, m_Tabs);
+        serializedObject.ApplyModifiedProperties();
+        GUILayout.Space(5);
         EditorGUILayout.EndVertical();
 
-        if (m_TabsIndex >= 0)
+        limits = inspectorBehaviour.limits;
+
+        if (tabIndex_Serialized.intValue >= 0)
         {
-            switch (m_Tabs[m_TabsIndex])
+            switch (m_Tabs[tabIndex_Serialized.intValue])
             {
-                case "Option One":
-                    EditorGUILayout.HelpBox("One", MessageType.Info);
+                case "Single Sound":
+                    if (inspectorBehaviour.soundClips.Length > 1)
+                    {
+                        EditorGUILayout.IntSlider(targetSound_Serialized, 0, inspectorBehaviour.soundClips.Length - 1);
+                        EditorGUILayout.ObjectField(inspectorBehaviour.soundClips[targetSound_Serialized.intValue], typeof(AudioClip));
+                    }
+                    else
+                    {
+                        EditorGUILayout.ObjectField(inspectorBehaviour.soundClips[0], typeof(AudioClip));
+                    }
+                    serializedObject.ApplyModifiedProperties();
                     break;
-                case "Option Two":
-                    EditorGUILayout.HelpBox("Two", MessageType.Info);
+                case "Multiple Sounds":
+                    if (inspectorBehaviour.soundClips.Length == 1)
+                    {
+                        EditorGUILayout.HelpBox("Only one sound is available. Please add more sounds.", MessageType.Info);
+                    }
+                    else
+                    {
+                        Debug.Log("soundClips.Length = " + inspectorBehaviour.soundClips.Length);
+                        for (int i = 0; i < inspectorBehaviour.soundClips.Length; i++)
+                        {
+                            //int inttest = 0;
+                            string nameOfField = inspectorBehaviour.soundClips[i].name;
+                            Debug.Log("nameOfField = " + nameOfField);
+                            limits[i] = EditorGUILayout.IntField(nameOfField, limits[i]);
+                        }
+                    }
+
                     break;
             }
         }
 
-        SimpleAudioPlayer inspectorBehaviour = (SimpleAudioPlayer)target;
 
         EditorGUI.BeginChangeCheck();
+
+        GameObject glorp = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/GameObject.prefab");
+        inspectorBehaviour.glorpington = glorp;
 
         gameObjectTest = inspectorBehaviour.gameObject;        
         
@@ -168,39 +193,31 @@ public class CustomInspectorEditor : Editor
 
         if (inspectorBehaviour.GetComponentInChildren<SimpleAudioPlayer_Child>() == null)
         {
-            EditorGUILayout.HelpBox("No Valid SAP Children were found! Use the button below generate one.", MessageType.Error);
+            EditorGUILayout.HelpBox("No Valid SAP Child was found! Use the button below generate one.", MessageType.Error);
             //Debug.LogError("No Valid SAP Children were found! Use the button inside of the script to generate one.");
-        }
 
-        if (GUILayout.Button("Test"))
-        {
-            if (instantiatedGameObject == null)
+            if (GUILayout.Button("Test"))
             {
-                instantiatedGameObject = (GameObject)PrefabUtility.InstantiatePrefab(inspectorBehaviour.emptyGameObject, inspectorBehaviour.transform);
-                inspectorBehaviour.SAP_Child = instantiatedGameObject;
+                if (instantiatedGameObject == null)
+                {
+                    instantiatedGameObject = (GameObject)PrefabUtility.InstantiatePrefab(glorp, inspectorBehaviour.transform);
+                    inspectorBehaviour.SAP_Child = instantiatedGameObject;
+                }
+                else
+                {
+                    Debug.LogWarning("A valid SAP Child has been detected. Please delete the existing SAP Child if you wish to regenerate");
+                }
             }
-            else
-            {
-                Debug.LogWarning("A valid SAP Child has been detected. Please delete the existing SAP Child if you wish to regenerate");
-            }
-        }
 
-        // A simple string field modification with Undo handling
-        //int test = EditorGUILayout.IntField("Num of Audio Clips", inspectorBehaviour.teddyClips.Length);
-        //string newStrVal = EditorGUILayout.TextField("String Val", inspectorBehaviour.stringVal);
-        bool boolTest = EditorGUILayout.Toggle("Bool", inspectorBehaviour.godTest);
-
-        if (boolTest)
-        {
-            EditorGUILayout.HelpBox("Testing", MessageType.Info);
         }
 
         if (EditorGUI.EndChangeCheck())
         {
             Undo.RecordObject(inspectorBehaviour, "Modify string val");
 
-            inspectorBehaviour.godTest = boolTest;
-            //inspectorBehaviour.stringVal = newStrVal;
+            inspectorBehaviour.targetSound = targetSound_Serialized.intValue;
+            inspectorBehaviour.limits = limits;
+
         }
     }
 }
--- SimpleAudioPlayer_Child.cs
+++ SimpleAudioPlayer_Child.cs
@@ -7,7 +7,7 @@ using VRC.Udon;
 public class SimpleAudioPlayer_Child : UdonSharpBehaviour
 {
     AudioSource SAPChild_AudioSource;
-    [SerializeField] AudioClip SAPChild_AudioClips;
+    [SerializeField] AudioClip[] SAPChild_AudioClips;
 
     [SerializeField] SimpleAudioPlayer SAP;
     [SerializeField] GameObject SAP_GameObject;
@@ -15,7 +15,10 @@ public class SimpleAudioPlayer_Child : UdonSharpBehaviour
     [SerializeField] public int test = 0;
 
     //[UdonSynced, FieldChangeCallback(nameof(RNGValueReciever))] public int test2 = 0;
-    [UdonSynced] public int test2 = 0;
+    [UdonSynced] public int RNGVal = 0;
+
+    [UdonSynced, FieldChangeCallback(nameof(limitsIn))] public int[] limits;
+    [SerializeField] public int maxRNGVal;
 
     void Start()
     {
@@ -24,40 +27,60 @@ public class SimpleAudioPlayer_Child : UdonSharpBehaviour
         SAPChild_AudioSource = GetComponent<AudioSource>();
     }
 
+    public int[] limitsIn
+    {
+        set
+        {
+            limits = value;
+            maxRNGVal = 0;
+            for (int i = 0; i < value.Length; i++)
+            {
+                maxRNGVal += value[i];
+            }
+        }
+        get { return limits; }
+    }
+
     public void GenerateRNGVal()
     {
         if (Networking.GetOwner(this.gameObject) != Networking.LocalPlayer)
         {
             Debug.Log("I am not the owner of this script");
+            return;
         }
-        test2 = Random.Range(0, 100);
+        //Random.Range 2nd parameter is maxExclusive, so +1 to it to capture full range of values
+        RNGVal = Random.Range(1, maxRNGVal + 1);
         RequestSerialization();
         OnDeserialization();
     }
 
-    //public int RNGValueReciever
-    //{
-    //    set
-    //    {
-    //        if (Networking.GetOwner(this.gameObject) != Networking.LocalPlayer)
-    //        {
-    //            return;
-    //        }
-
-    //        test2 = value;
-    //        RequestSerialization();
-    //        OnDeserialization();
-    //    }
-    //    get { return test2; }
-    //}
-
-    private void Update()
-    {
-        Debug.Log("test = " + test);
-    }
-
     public override void OnDeserialization()
     {
-        Debug.Log("RNG Val = " + test2);
+        Debug.Log("RNG Val = " + RNGVal);
+
+        int storedVal = 0;
+
+        for (int i = 0; i < limits.Length; i++)
+        {
+            for (int j = 1; j <= limits[i]; j++)
+            {
+                if ((j + storedVal) == RNGVal)
+                {
+                    // The .Length property returns the size of the array, but i index starts at 0, so add one to i to see if we are at an index greater than the length of the AudioClip array
+                    // i.e. if AudioClips had 3 objects in it, it's max index would be [2], but if i was 3 (the 4th index), .Length would return 3 and they would be equal
+                    // But if we index AudioClips[3], this would crash the script as we are indexing out of the array range, which is understandable
+                    // ...I'm not sure how often this would happen but I feel it's worth it to check just in case!
+                    if (i + 1 > SAPChild_AudioClips.Length)
+                    {
+                        Debug.LogError("Attempted to play sound outside array index range! Returning...");
+                        return;
+                    }
+
+                    SAPChild_AudioSource.PlayOneShot(SAPChild_AudioClips[i]);
+                    return;
+                }
+            }
+            storedVal += limits[i];
+        }
     }
 }
